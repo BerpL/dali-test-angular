927e4bdd3883a8e80c830e6d8b02cb9954c1f77b
Eder Cornejo <eder.cornejo@softit.pe>
2023-02-21 13:03:43 -0500

    test components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dali-test-2';
+  name = "Nombre del producto que mostraremos."
+  image = "https://res.cloudinary.com/dalidesignsystem/image/upload/v1653922649/storybook-assests/card-product/4508206_Marquesita_Vainilla_nbgkbc.jpg"
+  description = "Detalle de peso/unidad"
+  price = 17.44
+  discountPrice	= 0.58
+  stock = 254
+  width = "188px"
+  isOpen = false
+  
+  buttonAlert(){
+    this.isOpen = !this.isOpen
+  }
+
 }
